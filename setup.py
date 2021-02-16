8a82a00ff366205bb376a01e4e413e0b2e9966ed
Elijah Roberts <ejr@kount.com>
2021-02-16 11:25:45 -0700

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='gitlab-lint',
-    version='0.2.1',
+    version='0.3.0',
     py_modules=['gitlab-lint'],
     author="Elijah Roberts",
     author_email="elijah@elijahjamesroberts.com",
